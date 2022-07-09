841a182d1cf41f0469c9f72eb05b0dfd0e950000
gdsantana <guilherme30santana@gmail.com>
2022-07-09 16:21:01 -0300

    organizada no codigo

--- solucao.py
+++ solucao.py
@@ -7,9 +7,6 @@ lista_dir = []
 lista_abaixo = []
 
 class Nodo:
-    
-   
-
     def __init__(self, estado, pai, acao, custo):
      
         self.estado = estado
@@ -37,8 +34,6 @@ def sucessor(estado):
 
     return sucessores
     
-
-
 def expande(nodo):
     lista_nodos = []
     sucessores = sucessor(nodo.estado)
@@ -50,44 +45,6 @@ def expande(nodo):
         nodoSucessor = Nodo(estado,pai,acao,custo)
         lista_nodos.append(nodoSucessor)
     return lista_nodos
-
-
-def bfs(estado):
-    
-    t1 = time.time()
-    Nexpandidos = 0
-    fronteira = []
-    explorado = set()
-    nodo_inicial = Nodo(estado,None,None,1)
-    fronteira.append(nodo_inicial)
-    
-    
-    #print(fronteira)
-    
-    
-    while fronteira:          # Creating loop to visit each node
-        nodoAtual = fronteira[0]
-        fronteira.pop(0)
-
-        if nodoAtual.estado == "12345678_":
-            caminho = percorreCaminho(nodoAtual)
-            #print(caminho)
-            t2 = time.time()
-            tempo = t2 - t1
-            algoritmo = "BFS"
-            custo = len(caminho)
-            printa_resultado(algoritmo,tempo,Nexpandidos,custo)
-            return caminho
-
-            
-            
-        if nodoAtual.estado not in explorado:
-            explorado.add(nodoAtual.estado)
-            sucessores = expande(nodoAtual)
-            Nexpandidos = Nexpandidos + len(sucessores)
-            #for suc in sucessores:
-            #    fronteira.append(suc)
-            fronteira.extend(sucessores)
             
 
 def percorreCaminho(nodoAtual):
@@ -105,76 +62,6 @@ def printa_resultado(algoritmo,tempo,Nexpandidos,custo):
     print("Tempo de exec: " + str(tempo))
     print("Numero de nós expandidos: " + str(Nexpandidos))
     print("Custo: " + str(custo))
-            
-
-        
-        
-        
-        
-    
-      
-def dfs(estado):
-    t1= time.time()
-    NExpandidos = 0
-    pilha = []
-    expandidos = set()
-    nodoAtual = Nodo(estado,None,None,1)
-    expandidos.add(nodoAtual.estado)
-    
-
-    #tem que fazer a funçao de ve se tem solução ou não
-
-    while True:
-        sucessores = expande(nodoAtual)
-        NExpandidos = NExpandidos + len(sucessores)
-
-
-        for suc in sucessores:
-            if suc.estado not in expandidos:
-                pilha.append(suc)
-
-        
-        nodoAtual = pilha.pop()
-        expandidos.add(nodoAtual.estado)
-        
-
-        if(nodoAtual.estado == "12345678_"):
-            caminho = percorreCaminho(nodoAtual)
-            #print(caminho)
-            t2 = time.time()
-            tempo = t2 - t1
-            algoritmo = "DFS"
-            custo = len(caminho)
-            printa_resultado(algoritmo,tempo,NExpandidos,custo)
-            return caminho
-  
-  
-def astar_hamming(estado):
-    """"
-    Recebe um estado (string), executa a busca A* com h(n) = soma das distâncias de Hamming e
-    retorna uma lista de ações que leva do
-    estado recebido até o objetivo ("12345678_").
-    Caso não haja solução a partir do estado recebido, retorna None
-    :param estado: str
-    :return:
-    """
-    # substituir a linha abaixo pelo seu codigo
-    raise NotImplementedError
-
-
-def astar_manhattan(estado):
-    """
-    Recebe um estado (string), executa a busca A* com h(n) = soma das distâncias de Manhattan e
-    retorna uma lista de ações que leva do
-    estado recebido até o objetivo ("12345678_").
-    Caso não haja solução a partir do estado recebido, retorna None
-    :param estado: str
-    :return:
-    """
-    # substituir a linha abaixo pelo seu codigo
-    raise NotImplementedError
-
-
 
 
 def esquerda(Lestado,posunderline):
@@ -229,13 +116,120 @@ def acima(Lestado,posunderline):
         estado = "".join(auxLestado)
         return (mov,estado)
 
+def hamming_distance(s1, s2):
+    assert len(s1) == len(s2)
+    return sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2))
+
+#########################################################
+def bfs(estado):
+    
+    t1 = time.time()
+    Nexpandidos = 0
+    fronteira = []
+    explorado = set()
+    nodo_inicial = Nodo(estado,None,None,1)
+    fronteira.append(nodo_inicial)
+    
+    
+    #print(fronteira)
+    
+    
+    while fronteira:          # Creating loop to visit each node
+        nodoAtual = fronteira[0]
+        fronteira.pop(0)
+
+        if nodoAtual.estado == "12345678_":
+            caminho = percorreCaminho(nodoAtual)
+            #print(caminho)
+            t2 = time.time()
+            tempo = t2 - t1
+            algoritmo = "BFS"
+            custo = len(caminho)
+            printa_resultado(algoritmo,tempo,Nexpandidos,custo)
+            return caminho
+
+            
+            
+        if nodoAtual.estado not in explorado:
+            explorado.add(nodoAtual.estado)
+            sucessores = expande(nodoAtual)
+            Nexpandidos = Nexpandidos + len(sucessores)
+            #for suc in sucessores:
+            #    fronteira.append(suc)
+            fronteira.extend(sucessores)
+      
+def dfs(estado):
+    t1= time.time()
+    NExpandidos = 0
+    pilha = []
+    expandidos = set()
+    nodoAtual = Nodo(estado,None,None,1)
+    expandidos.add(nodoAtual.estado)
+    
+
+    #tem que fazer a funçao de ve se tem solução ou não
+
+    while True:
+        sucessores = expande(nodoAtual)
+        NExpandidos = NExpandidos + len(sucessores)
+
+
+        for suc in sucessores:
+            if suc.estado not in expandidos:
+                pilha.append(suc)
+
+        
+        nodoAtual = pilha.pop()
+        expandidos.add(nodoAtual.estado)
+        
+
+        if(nodoAtual.estado == "12345678_"):
+            caminho = percorreCaminho(nodoAtual)
+            #print(caminho)
+            t2 = time.time()
+            tempo = t2 - t1
+            algoritmo = "DFS"
+            custo = len(caminho)
+            printa_resultado(algoritmo,tempo,NExpandidos,custo)
+            return caminho
+  
+  
+def astar_hamming(estado):
+    """"
+    Recebe um estado (string), executa a busca A* com h(n) = soma das distâncias de Hamming e
+    retorna uma lista de ações que leva do
+    estado recebido até o objetivo ("12345678_").
+    Caso não haja solução a partir do estado recebido, retorna None
+    :param estado: str
+    :return:
+    """
+    # substituir a linha abaixo pelo seu codigo
+
+   
+
+def astar_manhattan(estado):
+    """
+    Recebe um estado (string), executa a busca A* com h(n) = soma das distâncias de Manhattan e
+    retorna uma lista de ações que leva do
+    estado recebido até o objetivo ("12345678_").
+    Caso não haja solução a partir do estado recebido, retorna None
+    :param estado: str
+    :return:
+    """
+    # substituir a linha abaixo pelo seu codigo
+    raise NotImplementedError
+
+
+
 
 
 
 if __name__ == "__main__":
   nodo_inicial = Nodo(estado_inicial,0,0,0)
-  
-  bfs_nodes=bfs(estado_inicial)
-  dfs_nodes=dfs(estado_inicial)
+  estadofinal="12345678"
+  print(estado_inicial.replace("_",''))
+  print(hamming_distance(estado_inicial.replace("_",''),estadofinal))
+  #bfs_nodes=bfs(estado_inicial)
+  #dfs_nodes=dfs(estado_inicial)
   #for n in bfs_nodes:
   #  n.print_nodo()
